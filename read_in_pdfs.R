5b92d38c1dac3e7b0b1bce909ea0eabffdb0fc72
SebastianKnigge <sebastian-knigge@live.de>
2019-08-09 12:07:54 +0200

    set up pdf import script

---
+++ read_in_pdfs.R
@@ -0,0 +1,37 @@
+# Script for reading in pdf files in the given eurostat format
+# install.packages("pdftools")
+
+getwd() %>% 
+  file.path("TextDocs") %>% 
+  setwd()
+documents <- list.files()
+
+documents
+
+library(pdftools)
+
+
+
+read_pdf_clean <- function(document){
+  # This function loads the document given per name
+  # and excludes the stop words inclusive numbers
+  pdf1 <- pdf_text(document) %>% 
+    strsplit(split = "\n") %>% 
+    do.call("c",.) %>% 
+    as_tibble() %>%
+    unnest_tokens(word,value)
+  
+  # load stopword library
+  data(stop_words)
+  # add own words to stop word library
+  new_stop_words <- tibble(word=as.character(1:10),lexicon=rep("own",10)) %>%
+    bind_rows(stop_words, own_stop_words)
+  
+  pdf1 %>% 
+    anti_join(new_stop_words)
+  
+  return(pdf1)
+}
+
+
+read_pdf_clean(documents[6]) %>% View
